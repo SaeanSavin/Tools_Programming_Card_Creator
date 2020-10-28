2b08578a3fe921a2f866f017b9894dc99b09ad7a
zamFe <zamfel18@student.kristiania.no>
2020-10-28 22:31:46 +0100

    Set up sqlserver code

---
+++ Classes/Db/CardContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Creator.Classes.Db
+{
+    public class CardContext : DbContext
+    {
+
+        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<CardType> CardTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server = (localdb)\MSSQLLocalDB; " +
+                @"Database = CardCreator; " +
+                @"Trusted_Connection = True; "
+                );
+        }
+    }
+}
